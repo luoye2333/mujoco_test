1f052451c3788e04e71b07a29bdd9f5068cefb43
luoye2333 <fallenleavz@gmail.com>
2025-08-02 19:19:53 +0800

    init

---
+++ test.py
@@ -0,0 +1,28 @@
+import time
+import numpy as np
+import mujoco
+import mujoco.viewer
+
+model = mujoco.MjModel.from_xml_path("test.xml")
+model.opt.timestep = 0.02
+data = mujoco.MjData(model)
+
+
+# path = "stair_up.npy"
+path = "stair_down.npy"
+vertical_scale = 0.005
+real_height = np.load(path) * vertical_scale
+
+model.hfield_data = real_height.transpose().flatten() # penetrate
+# model.hfield_data = real_height.transpose().flatten() * 10 # penetrate more
+# model.hfield_data = real_height.transpose().flatten() * 0.1 # ok
+# model.hfield_data = real_height.transpose().flatten() - 2.0
+
+viewer = mujoco.viewer.launch_passive(model, data)
+
+while viewer.is_running():
+    mujoco.mj_step(model, data)
+    time.sleep(1/50.)
+    viewer.sync()
+
+viewer.close()
